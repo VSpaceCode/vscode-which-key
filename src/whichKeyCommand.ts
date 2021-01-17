78cd3c3ec9f1e4780e66c90b43b5a55717d869fb
Steven Guh <stevenguh@gmail.com>
2021-01-16 20:23:17 -0800

    Refactor the override and sort item to create a canonical bindings

--- src/whichKeyCommand.ts
+++ src/whichKeyCommand.ts
@@ -1,11 +1,11 @@
 import { Disposable, workspace } from "vscode";
 import { BindingItem, OverrideBindingItem } from "./bindingItem";
-import { ConfigKey, contributePrefix, SortOrder } from "./constants";
+import { ConfigKey, Configs, contributePrefix, SortOrder } from "./constants";
 import KeyListener from "./keyListener";
 import { WhichKeyMenu } from "./menu/menu";
 import { BaseMenuItem, RootMenuItem } from "./menu/menuItem";
 import { IStatusBar } from "./statusBar";
-import { getFullSection, WhichKeyConfig } from "./whichKeyConfig";
+import { WhichKeyConfig } from "./whichKeyConfig";
 
 export default class WhichKeyCommand {
     private statusBar: IStatusBar;
@@ -22,27 +22,18 @@ export default class WhichKeyCommand {
         this.unregister();
         this.config = config;
 
-        const bindings = getConfig<BindingItem[]>(config.bindings);
+        const bindings = getCanonicalConfig(config);
         if (bindings) {
             this.root = new RootMenuItem(bindings);
         }
 
-        if (config.overrides) {
-            const overrides = getConfig<OverrideBindingItem[]>(config.overrides) ?? [];
-            this.root?.override(overrides);
-        }
-
-        const sortOrder = workspace
-            .getConfiguration(contributePrefix)
-            .get<SortOrder>(ConfigKey.SortOrder) ?? SortOrder.None;
-        this.root?.sortItems(sortOrder);
-
         this.onConfigChangeListener = workspace.onDidChangeConfiguration((e) => {
             if (
-                e.affectsConfiguration(getFullSection([contributePrefix, ConfigKey.SortOrder])) ||
+                e.affectsConfiguration(Configs.SortOrder) ||
                 e.affectsConfiguration(config.bindings) ||
                 (config.overrides && e.affectsConfiguration(config.overrides))
             ) {
+                this.unregister();
                 this.register(config);
             }
         }, this);
@@ -75,11 +66,129 @@ function showMenu(statusBar: IStatusBar, keyListener: KeyListener, items: BaseMe
 
 function getConfig<T>(section: string) {
     // Get the minimal scope
+    let filterSection: string | undefined = undefined;
+    let lastSection: string = section;
     const idx = section.lastIndexOf('.');
-    if (idx === -1) {
-        return workspace.getConfiguration().get<T>(section);
+    if (idx !== -1) {
+        filterSection = section.substring(0, idx);
+        lastSection = section.substring(idx + 1);
+    }
+
+    return workspace.getConfiguration(filterSection).get<T>(lastSection);
+}
+
+function getSortOrder() {
+    return getConfig<SortOrder>(Configs.SortOrder) ?? SortOrder.None;
+}
+
+function getCanonicalConfig(c: WhichKeyConfig) {
+    const bindings = getConfig<BindingItem[]>(c.bindings) ?? [];
+    if (c.overrides) {
+        const overrides = getConfig<OverrideBindingItem[]>(c.overrides) ?? [];
+        overrideBindingItems(bindings, overrides);
+    }
+
+    const sortOrder = getSortOrder();
+    sortBindingsItems(bindings, sortOrder);
+
+    return bindings;
+}
+
+function convertOverride(key: string, o: OverrideBindingItem) {
+    if (o.name !== undefined && o.type !== undefined) {
+        return {
+            key: key,
+            name: o.name,
+            type: o.type,
+            command: o.command,
+            commands: o.commands,
+            args: o.args,
+            bindings: o.bindings,
+        };
     } else {
-        return workspace.getConfiguration(section.substring(0, idx))
-            .get<T>(section.substring(idx + 1));
+        throw new Error('name or type of the override is undefined');
+    }
+}
+
+function findBindings(items: BindingItem[], keys: string[]) {
+    // Traverse to the last level
+    let bindingItems: BindingItem[] | undefined = items;
+    for (let i = 0; i < keys.length - 1; i++) {
+        const key = keys[i];
+        const keyIndex: number | undefined = bindingItems?.findIndex(i => i.key === key);
+        if (keyIndex === undefined || keyIndex === -1) {
+            console.warn(`Key ${key} of ${keys.toString()} not found`);
+            break;
+        }
+        bindingItems = bindingItems?.[keyIndex]?.bindings;
+    }
+
+    return bindingItems;
+}
+
+function overrideBindingItems(items: BindingItem[], overrides: OverrideBindingItem[]) {
+    for (const o of overrides) {
+        try {
+            const keys = (typeof o.keys === 'string') ? o.keys.split('.') : o.keys;
+            const bindingItems = findBindings(items, keys);
+
+            if (bindingItems !== undefined) {
+                const key = keys[keys.length - 1]; // last Key
+                const index = bindingItems.findIndex(i => i.key === key);
+
+                if (o.position === undefined) {
+                    const newItem = convertOverride(key, o);
+                    if (index !== -1) {
+                        // replace the current item
+                        bindingItems.splice(index, 1, newItem);
+                    } else {
+                        // append if there isn't an existing binding
+                        bindingItems.push(newItem);
+                    }
+                } else {
+                    if (o.position < 0) {
+                        // negative position, attempt to remove
+                        if (index !== -1) {
+                            bindingItems.splice(index, 1);
+                        }
+                    } else {
+                        // Remove and replace
+                        if (index !== -1) {
+                            bindingItems.splice(index, 1);
+                        }
+                        const newItem = convertOverride(key, o);
+                        bindingItems.splice(o.position, 0, newItem);
+                    }
+                }
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+// in place sort
+function sortBindingsItems(items: BindingItem[] | undefined, order: SortOrder) {
+    if (!items || order === SortOrder.None) {
+        return;
+    }
+
+    if (order === SortOrder.Alphabetically) {
+        items.sort((a, b) => a.key.localeCompare(b.key));
+    } else if (order === SortOrder.NonNumberFirst) {
+        items.sort((a, b) => {
+            const regex = /^[0-9]/;
+            const aStartsWithNumber = regex.test(a.key);
+            const bStartsWithNumber = regex.test(b.key);
+            if (aStartsWithNumber !== bStartsWithNumber) {
+                // Sort non-number first
+                return aStartsWithNumber ? 1 : -1;
+            } else {
+                return a.key.localeCompare(b.key);
+            }
+        });
+    }
+    for (const item of items) {
+        sortBindingsItems(item.bindings, order);
     }
 }
