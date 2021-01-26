6ea572efe9e45b8243276a63d7b7d0b2075f510c
Steven Guh <stevenguh@gmail.com>
2021-01-26 00:20:32 -0800

    Add CharCode const enum

---
+++ src/charCode.ts
@@ -0,0 +1,38 @@
+export const enum CharCode {
+    /**
+     * The `\t` character.
+     */
+    Tab = 9,
+    /**
+     * The ` ` character.
+     */
+    Space = 32,
+    /**
+     * The `!` character.
+     */
+    Exclamation = 33,
+    /**
+	 * The `/` character.
+	 */
+    Slash = 47,
+	/**
+	 * The `:` character.
+	 */
+    Colon = 58,
+
+    A = 65,
+    Z = 90,
+
+    /**
+     * The `\` character.
+     */
+    Backslash = 92,
+
+    a = 97,
+    z = 122,
+
+    /**
+     * The `~` character.
+     */
+    Tide = 126
+}
