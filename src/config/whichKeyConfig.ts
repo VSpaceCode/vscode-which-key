ab0f8fc49e680c713392c4b76e7b4f750e7d99b8
Steven Guh <stevenguh@gmail.com>
2021-05-23 18:00:45 -0700

    Simplify old WhichKeyConfig handling

--- src/config/whichKeyConfig.ts
+++ src/config/whichKeyConfig.ts
@@ -1,11 +1,6 @@
 import { Configs } from "../constants";
 
 type ConfigSections = [string, string];
-export interface OldWhichKeyConfig {
-    bindings: ConfigSections;
-    overrides?: ConfigSections;
-    title?: string;
-}
 
 export interface WhichKeyConfig {
     bindings: string;
@@ -21,13 +16,7 @@ function isConfigSections(x: any): x is ConfigSections {
     return x && Array.isArray(x) && x.length === 2 && isString(x[0]) && isString(x[1]);
 }
 
-function isOldWhichKeyConfig(config: any): config is OldWhichKeyConfig {
-    return (config.bindings && isConfigSections(config.bindings)) &&
-        (!config.overrides || isConfigSections(config.overrides)) &&
-        (!config.title || isString(config.title));
-}
-
-function isWhichKeyConfig(config: any): boolean {
+function isWhichKeyConfig(config: any): config is WhichKeyConfig {
     return (config.bindings && isString(config.bindings)) &&
         (!config.overrides || isString(config.overrides)) &&
         (!config.title || isString(config.title));
@@ -37,21 +26,13 @@ function getFullSection(sections: ConfigSections): string {
     return `${sections[0]}.${sections[1]}`;
 }
 
-function convertOldWhichKeyConfig(o: OldWhichKeyConfig): WhichKeyConfig {
-    const config: WhichKeyConfig = {
-        bindings: getFullSection(o.bindings),
-        title: o.title
-    };
-    if (o.overrides) {
-        config.overrides = getFullSection(o.overrides);
-    }
-    return config;
-}
-
 export function toWhichKeyConfig(o: any): WhichKeyConfig | undefined {
     if (typeof o === "object") {
-        if (isOldWhichKeyConfig(o)) {
-            return convertOldWhichKeyConfig(o);
+        if (o.bindings && isConfigSections(o.bindings)) {
+            o.bindings = getFullSection(o.bindings);
+        }
+        if (o.overrides && isConfigSections(o.overrides)) {
+            o.overrides = getFullSection(o.overrides);
         }
         if (isWhichKeyConfig(o)) {
             return o;
