a5bba43013ef0e475c2351fdaf7a50641a186442
Michael Ye <mly32@cornell.edu>
2022-02-04 14:37:29 -0500

    add prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'prettier'
     ],
     rules: {
         'semi': [2, 'always'],
